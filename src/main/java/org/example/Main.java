9100244b76109da06e5d7e10987370e4008662aa
Krzysztof Sroczyk <krzysztof.sroczyk@openx.com>
2024-04-04 11:34:39 +0200

    Example usage

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -4,7 +4,7 @@ public class Main {
     public static void main(String[] args) {
         Animal kangur = Animal.builder().setName("kangur").setNumberOfLegs(2).build();
         System.out.println(kangur.name());
-        Animal los = kangur.toBuilder().setName("Łoś").setNumberOfLegs(4).build();
+        Animal los = kangur.toBuilder().setName("Łoś").build();
         System.out.println(los.name());
     }
 }
